69499bfbd1876232f2c8e6b92298ed9ab44b513d
turkersenturk <turkersenturk@gmail.com>
2018-01-17 17:34:33 +0300

    Update AppHeader.vue
    denemeeee

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,7 @@
   <div class="hero-body">
     <div class="container">
       <p class="title is-3">
-        Captain Reader
+        Captain Reader by Mehmet Kaptan
       </p>
       <p class="subtitle is-5">Read faster!</p>
     </div>
